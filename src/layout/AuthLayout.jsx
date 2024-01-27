9358aca86d08e050d0671540a534db5e7174ca38
Lucas <lucassarachu7@gmail.com>
2024-01-27 14:16:43 +0100

    Agregando sweetAlert 2

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,5 +1,28 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom"
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 const AuthLayout = () => {
+
+  useEffect(() => {
+    Swal.fire({
+      icon: "info",
+      title: "¡Bienvenido a la App para Administrar tus Pacientes!",
+      text: "Correo de prueba: correo@correo.com , Password de prueba: password"
+     
+    });
+    // const MySwal = withReactContent(Swal);
+
+    // MySwal.fire({
+    //   title: <p>¡Bienvenido a la App para Administrar tus Pacientes!</p>,
+    //   didOpen: () => {
+    //     MySwal;
+    //   },
+    // }).then(() => {
+    //   MySwal.fire(<p>Shorthand works too</p>);
+    // });
+  }, []); 
+
   return (
     <>
         <main className="container items-center mx-auto md:grid md:grid-cols-2 mt-11 gap-10 p-5">
